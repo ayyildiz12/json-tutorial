8e388e4e6ade9e60b2961a4cd279dd930b59c3a0
荒原狼 <84221282+ayyildiz12@users.noreply.github.com>
2021-09-29 21:04:01 +0800

    Update leptjson.h

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,12 +1,17 @@
+#pragma once
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
+
+//先枚举lept_type六种数据类型
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
+//声明jason数据结构，一个jason节点
 typedef struct {
     lept_type type;
 }lept_value;
 
+//枚举返回值类型
 enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
@@ -14,8 +19,11 @@ enum {
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
+//声明两个函数
+//解析jason函数
 int lept_parse(lept_value* v, const char* json);
 
+//访问结果函数，获取他的类型
 lept_type lept_get_type(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
